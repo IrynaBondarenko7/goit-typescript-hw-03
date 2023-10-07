b91f1fb24d721a4e01580e44471947129179fefa
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-10-07 21:48:47 +0100

    four task is done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
 
-const house = new MyHouse(key);
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    const newKey = this.key.getSignature();
+    return newKey;
+  }
+}
+
+abstract class House {
+  door = true || false;
+  key: Key;
+  tenants: Person[] = [];
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key === this.key) {
+      this.door = true;
+      console.log("Двері відчинено.");
+    } else {
+      console.log("Неправильний ключ. Двері не відкриються.");
+    }
+  }
+}
+
+const key = new Key(25);
+
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
